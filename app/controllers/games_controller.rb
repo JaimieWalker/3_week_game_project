0f0c85c33236cecd74d224fe7cc7734a92234635
gil93 <gvelezjr02@gmail.com>
2015-10-21 11:02:52 -0400

    ajax chat

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,12 @@
 class GamesController < ApplicationController
   before_action :require_user, only: [:index]
 
-  def index; end
+  def index
+    @user = current_user
+
+      respond_to do |format|
+        format.html
+        format.json { render json: @user }
+      end
+  end
 end
