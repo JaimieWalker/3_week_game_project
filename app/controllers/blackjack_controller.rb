2313901815a812d9617233ba1a39f4112865e0df
JaimieWalker <jaimie.walker90@gmail.com>
2015-10-23 16:09:40 -0400

    Database of cards is filled

--- app/controllers/blackjack_controller.rb
+++ app/controllers/blackjack_controller.rb
@@ -1,18 +1,26 @@
 class BlackjackController < ApplicationController
-  def start
+  def create
 
     @cards = Dir["*/assets/images/*.png"]
     #Create Regex to assign a value to a card. And fill the database with it if they do not have
-    #what we want 
+    #what we want
 
     if Card.all.count < 52
-      @cards.each { |card| card.create() } 
+      @cards.each do |card|
+        value = card.split("app/assets/images/").pop.split("_of_").shift
+        if value == "ace"
+          Card.create(name:card,value:11)
+        elsif value == ("king" || "queen" || "jack")
+          Card.create(name:card,value:10)
+        else
+          Card.create(name:card,value:value.to_i)
+        end
+      end
     end
-    # "app/assets/images/king_of_diamonds.png"
   end
 
 
-  # def index 
+  # def index
 
   # @game = Game.new
   #   # binding.pry
@@ -20,22 +28,22 @@ class BlackjackController < ApplicationController
   #   @game.dealer
   # end
 
-  # def hit 
-  #  @deal = true  
+  # def hit
+  #  @deal = true
   #    render :index
   # end
 
 
 
 
-# def hit
-#   cards_in_hand += new_card
-#   num_check
-# end
+  # def hit
+  #   cards_in_hand += new_card
+  #   num_check
+  # end
 
-# def num_check
-#    if @sum > 21 
-#     game_over
-# end
+  # def num_check
+  #    if @sum > 21
+  #     game_over
+  # end
 
 end
