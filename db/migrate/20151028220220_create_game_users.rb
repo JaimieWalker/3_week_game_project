0d8c20766fec39305553aec0aa82134c9ed3dc91
JaimieWalker <jaimie.walker90@gmail.com>
2015-10-28 18:33:33 -0400

    Saving my work

---
+++ db/migrate/20151028220220_create_game_users.rb
@@ -0,0 +1,10 @@
+class CreateGameUsers < ActiveRecord::Migration
+  def change
+    create_table :game_users do |t|
+      t.belongs_to :game, index: true
+      t.belongs_to :user, index: true
+
+      t.timestamps null: false
+    end
+  end
+end
